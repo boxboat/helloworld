05f03f0931108c4a2e6101a032ce906503481290
Cole Kennedy <colek42@users.noreply.github.com>
2018-09-26 16:15:27 -0400

    Update main.go

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	log.Println("Starting helloworld application...")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello VPay!!\n")
+		fmt.Fprintf(w, "Hello VPay Is it Happy hour?!!\n")
 	})
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
